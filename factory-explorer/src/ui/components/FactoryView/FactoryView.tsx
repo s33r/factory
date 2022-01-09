fe11caa90a744b605b6bc3f5f30f0bd186f8d3a8
Aaron Willows <aaron@aaronwillows.com>
2022-01-08 16:00:05 -0800

    Implmented basic filtering

--- factory-explorer/src/ui/components/FactoryView/FactoryView.tsx
+++ factory-explorer/src/ui/components/FactoryView/FactoryView.tsx
@@ -18,6 +18,7 @@ import SupplyTableView from '../SupplyTableView/SupplyTableView';
 
 import { recipeLibrary } from '../../../data';
 import Factory from '../../../model/Factory';
+import { SimpleMaker } from '../../../data/simpleTypes';
 import RecipeCard from '../RecipeCard/RecipeTable';
 
 const allRecipes  = new Factory('All Recipes');
@@ -28,64 +29,95 @@ type FactoryViewProps = {
   factory: Factory
 }
 
-function FactoryView(props: FactoryViewProps) {
-  return (
-    <div>
-      <h2>Factory: {props.factory.name}</h2>
-
-      <Container id="action-row" fluid>
-
-        <Button variant="primary">Download Factory</Button>
-
-      </Container>
-
-      <Tabs defaultActiveKey="factory-view" id="main-tabs" className="mb-3">
-        <Tab eventKey="factory-view" title="Buildings">
-          <RecipeTable factory={props.factory}></RecipeTable>
-        </Tab>
-        <Tab eventKey="io-view" title="I/O">
-          <SupplyTableView factory={props.factory}/>
-        </Tab>
-        <Tab eventKey="all-view" title="All Recipes">
-          <Container fluid>
-            <InputGroup className="mb-3">
-            <DropdownButton
-              variant="outline-secondary"
-              title="Filter Type"
-              id="filter-type-selector">
-
-              <Dropdown.Item href="#">Recipe Name</Dropdown.Item>
-              <Dropdown.Item href="#">Outputs</Dropdown.Item>
-              <Dropdown.Item href="#">Inputs</Dropdown.Item>
-            </DropdownButton>
-            <InputGroup.Text id="recipe-filter">Filter</InputGroup.Text>
-            <FormControl
-              placeholder="Recipe Name"
-              aria-label="Filter Text"
-              aria-describedby="recipe-filter"
-              // onChange={filterList}
-              />
-            </InputGroup>
-
-            <Container fluid>
-              <Row xs={1} md={2} lg={4} className="g-4">
-                  {recipeLibrary.getAll().map((entry,index) => (
-                    <Col key={index}>
-                      <RecipeCard recipe={entry}></RecipeCard>
-                    </Col>
-                  ))}
-              </Row>
-            </Container>
-
-          </Container>
-
-
-        </Tab>
-
-      </Tabs>
-    </div>
-
-  );
+type FactoryViewState = {
+  recipeList: SimpleMaker[],
+  filterType: 'name',
 }
 
-export default FactoryView;
+
+
+export default class FactoryView extends React.Component<FactoryViewProps, FactoryViewState> {
+
+  readonly state: FactoryViewState;
+
+  constructor(props: FactoryViewProps) {
+    super(props);
+
+    this.state = {
+      recipeList: recipeLibrary.getAll()
+    }
+  }
+
+  filterList(event: any) {
+    const filter = event.target.value;
+
+    if(!filter) {
+      this.setState({
+        recipeList: recipeLibrary.getAll()
+      });
+    } else {
+      this.setState({
+        recipeList: recipeLibrary.filterByName(filter)
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Factory: {this.props.factory.name}</h2>
+
+        <Container id="action-row" fluid>
+
+          <Button variant="primary">Download Factory</Button>
+
+        </Container>
+
+        <Tabs defaultActiveKey="factory-view" id="main-tabs" className="mb-3">
+          <Tab eventKey="factory-view" title="Buildings">
+            <RecipeTable factory={this.props.factory}></RecipeTable>
+          </Tab>
+          <Tab eventKey="io-view" title="I/O">
+            <SupplyTableView factory={this.props.factory}/>
+          </Tab>
+          <Tab eventKey="all-view" title="All Recipes">
+            <Container fluid>
+              <InputGroup className="mb-3">
+              <DropdownButton
+                variant="outline-secondary"
+                title="Filter Type"
+                id="filter-type-selector">
+
+                <Dropdown.Item href="#">Recipe Name</Dropdown.Item>
+                <Dropdown.Item href="#">Outputs</Dropdown.Item>
+                <Dropdown.Item href="#">Inputs</Dropdown.Item>
+              </DropdownButton>
+              <FormControl
+                placeholder="Recipe Name"
+                aria-label="Filter Text"
+                aria-describedby="recipe-filter"
+                onChange={this.filterList.bind(this)}
+                />
+              </InputGroup>
+
+              <Container fluid>
+                <Row xs={1} md={2} lg={4} className="g-4">
+                    {this.state.recipeList.map((entry,index) => (
+                      <Col key={index}>
+                        <RecipeCard recipe={entry}></RecipeCard>
+                      </Col>
+                    ))}
+                </Row>
+              </Container>
+
+            </Container>
+
+
+          </Tab>
+
+        </Tabs>
+      </div>
+
+    );
+  }
+}
